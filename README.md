cc8106c86bd19e8571662e5830348b016f7e576e
Andy <hinandy@gmail.com>
2018-11-09 15:00:10 -0500

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Requirements
+- Python 2.7 
+
+# Running
+- `python run.py`
+    - Verbose: `python run.py --verbose`
+    - FFMPEG path: `python run.py --ffmpeg <path_to_ffmpeg>`
+- Output will be saved to the `output/` directory.
+
+# Adding more tests
+- Add new images to test by adding them to the `media/` directory.
+- Add new tests by modifying the `TESTS` array in the `run.py` file.
---
+++ run.py
@@ -0,0 +1,70 @@
+import os
+from argparse import ArgumentParser
+import subprocess
+import shutil
+
+#### CLI ARGS #################################
+
+parser = ArgumentParser()
+parser.add_argument("-f", "--ffmpeg", dest="ffmpeg", default=None, help="Path to ffmpeg")
+parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true", help="Verbose logs")
+args = parser.parse_args()
+
+#### CONSTANTS #################################
+
+MEDIA_PATH = "./media/"
+OUTPUT_PATH = "./output"
+DEV_NULL = open(os.devnull, 'wb')
+
+TESTS = [
+    {
+        "name": "resize",
+        "cmd": "-filter:v scale=iw/2:-2"
+    },
+    {
+        "name": "crop",
+        "cmd": "-filter:v crop=iw/2:ih/2:iw/4:ih/4"
+    }
+]
+
+#### HELPERS ###################################
+
+def run_tests(file_path):
+    num_tests = len(TESTS)
+
+    for i, test_data in enumerate(TESTS):
+        print("%d of %d (%s)" % (i+1, num_tests, test_data["name"]))
+        
+        ffmpeg_path = args.ffmpeg if args.ffmpeg else "ffmpeg"
+
+        base_name = os.path.basename(file_path)
+        file_name, ext = os.path.splitext(base_name)
+        dest_path = "%s/%s_%s%s" % (OUTPUT_PATH, file_name, test_data["name"], ext)
+        full_cmd = "%s -i %s %s -y %s" % (ffmpeg_path, file_path, test_data["cmd"], dest_path)
+
+        if args.verbose:
+            subprocess.call(full_cmd, shell=True)
+        else:
+            subprocess.call(full_cmd, shell=True, stderr=DEV_NULL, stdout=DEV_NULL)
+
+
+#### MAIN #####################################
+
+# Delete anything in the output dir
+if os.path.isdir(OUTPUT_PATH):
+    shutil.rmtree(OUTPUT_PATH) 
+
+os.mkdir(OUTPUT_PATH)
+
+files = os.listdir(MEDIA_PATH)
+
+for file_name in files:
+    file_path = MEDIA_PATH + file_name
+
+    print("-- Running Tests for %s --" % (file_path))
+    
+    run_tests(file_path)
+
+    print("")
+    print("")
+
